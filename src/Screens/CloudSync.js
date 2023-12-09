2b9c9d6a857dc1243135d990d2dacef4a56d8d59
azaiez hammadi <azaiezhammadi@outlook.com>
2023-12-09 23:21:03 +0100

    FEAT : offline json files backup / restore

--- src/Screens/CloudSync.js
+++ src/Screens/CloudSync.js
@@ -12,6 +12,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { setData, updateCumulativePaths } from "../Store/dataSlice";
 import { readObject, storeObject } from "../Utils/asyncStorage";
 
+import * as FileSystem from "expo-file-system";
+import * as DocumentPicker from "expo-document-picker";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCaENsDDIVdz7erHN05N6VpTdHkdUlntCU",
   authDomain: "enig-lab264.firebaseapp.com",
@@ -53,8 +56,8 @@ function CloudSync() {
       console.log(error);
 
       // just to hide the "firebase" word in the alert shown to the user :D
-      let errorToShow=error.message;
-      if(errorToShow.startsWith("Firebase")) errorToShow=errorToShow.substring("Firebase".length +2);
+      let errorToShow = error.message;
+      if (errorToShow.startsWith("Firebase")) errorToShow = errorToShow.substring("Firebase".length + 2);
       alert("Sign in failed: " + errorToShow);
     } finally {
       setLoading(false);
@@ -141,9 +144,70 @@ function CloudSync() {
     });
   }, []);
 
+  const exportToJson = async () => {
+    try {
+      setLoading(true);
+      const jsonString = JSON.stringify({a:"t"});
+      // Let the user pick a directory to save the file
+      const directoryUri = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
+      if (directoryUri.granted) {
+        // Create a new file in the selected directory
+        const newFileUri = await FileSystem.StorageAccessFramework.createFileAsync(directoryUri.directoryUri, "exportedData.json", "application/json");
+        // Write the JSON string to the new file
+        await FileSystem.writeAsStringAsync(newFileUri, jsonString, { encoding: FileSystem.EncodingType.UTF8 });
+        alert("JSON file exported. Path: " + newFileUri);
+        console.log("JSON file exported successfully. Path:", newFileUri);
+      } else {
+        alert("Exporting error: Directory permissions were denied");
+        console.log("Directory permissions were denied");
+      }
+    } catch (error) {
+      console.error("Error exporting JSON file:", error.message);
+      alert("Error exporting JSON file:", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const importFromJson = async () => {
+    try {
+      setLoading(true);
+      const result = await DocumentPicker.getDocumentAsync({ type: "application/json" });
+
+      // Check if the document picker was not cancelled and assets are available
+      if (!result.cancelled && result.assets && result.assets.length > 0) {
+        const { uri } = result.assets[0]; // Get the URI of the selected file
+        const jsonString = await FileSystem.readAsStringAsync(uri);
+        const importedData = JSON.parse(jsonString);
+        const validation = validateJsonStructure(importedData);
+        if (validation.isValid) {
+          await storeObject("data", importedData);
+          dispatch(setData(importedData));
+          dispatch(updateCumulativePaths());
+          console.log("The JSON object has the correct form and can be used.");
+          alert("JSON file imported successfully");
+        } else {
+          console.error("The JSON object does not match the required form:", validation.errorMessage);
+          alert("JSON file is not valid : "+validation.errorMessage);
+        }
+        console.log("JSON file imported successfully:", importedData);
+
+
+      } else {
+        console.log("Document picker was cancelled or no file was selected");
+        alert("Importing error: operation cancelled or no file was selected");
+      }
+    } catch (error) {
+      console.error("Error importing JSON file:", error.message);
+      alert("Error importing JSON file:", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   if (!isLoggedIn)
     return (
-      <View className="flex-1 flex justify-center items-center gap-6">
+      <View className="flex-1 flex justify-center items-center gap-7">
         <View>
           <View className="flex flex-row gap-2  justify-start mb-2 ">
             <Text className="text-xl w-4/12">UserName :</Text>
@@ -156,6 +220,15 @@ function CloudSync() {
         </View>
         <TouchableOpacity onPress={signIn} className={"flex justify-center items-center w-40 h-12 " + (loading ? "bg-gray-300" : "bg-rose-600")} disabled={loading}>
           <Text className="text-xl text-white font-bold">Sign in</Text>
+        </TouchableOpacity>
+
+        <Text className="text-xl text-black font-bold pt-16">Offline data management</Text>
+
+        <TouchableOpacity onPress={exportToJson} className={"flex justify-center items-center w-44 h-12 " + (loading ? "bg-gray-300" : "bg-rose-600")} disabled={loading}>
+          <Text className="text-xl text-white font-bold">Export as JSON</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={importFromJson} className={"flex justify-center items-center w-44 h-12 mb-48 " + (loading ? "bg-gray-300" : "bg-rose-600")} disabled={loading}>
+          <Text className="text-xl text-white font-bold">Import JSON</Text>
         </TouchableOpacity>
       </View>
     );
@@ -180,3 +253,51 @@ function CloudSync() {
 }
 
 export default CloudSync;
+
+
+const validateJsonStructure = (jsonObject) => {
+  let isValid = true;
+  let errorMessage = '';
+
+  if (!jsonObject.hasOwnProperty('type')) {
+    errorMessage += 'Property "type" is missing.\n';
+    isValid = false;
+  } else if (jsonObject.type !== 'container') {
+    errorMessage += 'Property "type" must be "container".\n';
+    isValid = false;
+  }
+
+  if (!jsonObject.hasOwnProperty('path')) {
+    errorMessage += 'Property "path" is missing.\n';
+    isValid = false;
+  } else if (typeof jsonObject.path !== 'string') {
+    errorMessage += 'Property "path" must be a string.\n';
+    isValid = false;
+  }
+
+  if (!jsonObject.hasOwnProperty('cumulativePath')) {
+    errorMessage += 'Property "cumulativePath" is missing.\n';
+    isValid = false;
+  } else if (typeof jsonObject.cumulativePath !== 'string') {
+    errorMessage += 'Property "cumulativePath" must be a string.\n';
+    isValid = false;
+  }
+
+  if (!jsonObject.hasOwnProperty('separator')) {
+    errorMessage += 'Property "separator" is missing.\n';
+    isValid = false;
+  } else if (jsonObject.separator !== false) {
+    errorMessage += 'Property "separator" must be false.\n';
+    isValid = false;
+  }
+
+  if (!jsonObject.hasOwnProperty('Data')) {
+    errorMessage += 'Property "Data" is missing.\n';
+    isValid = false;
+  } else if (!Array.isArray(jsonObject.Data) || jsonObject.Data.length === 0) {
+    errorMessage += 'Property "Data" must be a non-empty array.\n';
+    isValid = false;
+  }
+
+  return { isValid, errorMessage };
+};
